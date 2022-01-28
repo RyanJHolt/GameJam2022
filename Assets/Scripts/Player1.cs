67362f21be26305de663bb882cca48c3cef95300
Ryan Holt <44439777+RyanJHolt@users.noreply.github.com>
2022-01-28 23:11:27 +0000

    idk man

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class Player1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
