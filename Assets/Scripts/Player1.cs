82d61e37ba0cb71c66b34457f047b8bc7085aa0d
Ryan Holt <44439777+RyanJHolt@users.noreply.github.com>
2022-01-29 00:02:23 +0000

    Added Player controller

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -1,20 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
 public class Player1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float moveSpeed = 5;
+    public float jumpHeight = 2;
+    public float jumps = 2;
+    public float jumpWait = 0;
+    
+    
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * (moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * (-moveSpeed * Time.deltaTime);
+            print(Time.deltaTime.ToString());
+
+        }
+
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space) ) && jumps != 0 && jumpWait == 0)
+        {
+            transform.Translate(Vector3.up);
+            jumps -= 1;
+            jumpWait = 60;
+
+        }
+
+        if (jumpWait > 0)
+        {
+            jumpWait -= 1;
+        }
     }
 }
